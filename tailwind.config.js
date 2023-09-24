0970c64e749c113f639c2c0509b8a1004f642be0
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-24 13:03:01 -0700

    Added custom fonts and fixe the tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          main: "#FF3BFF ",
+          secondary: "#5C24FF",
+          tartary: "#D94FD5",
+        },
+        white: {
+          main: "#fff",
+        },
+        black: {
+          main: "#000",
+        },
+      },
+      fontFamily: {
+        clash: ["Space", "sans-serif"],
+        space: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
