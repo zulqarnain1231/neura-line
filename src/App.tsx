f614f0ea8d4470edb2f172df9b7a7faea99ab262
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-25 01:09:51 -0700

    finished hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Navbar from "./components/Shared/Navigations/Navbar";
+import Hero from "./components/Home/Hero";
 function App() {
   return (
     <main className="font-clash bg-gradient-to-b from-black-main to-[#3D0C3D]">
       <Navbar />
+      <Hero />
     </main>
   );
 }
